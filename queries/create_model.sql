db9167a0802b0301dfc0208f1793a6a99a254e7e
3y3c0d3 <97314133+3y3c0d3@users.noreply.github.com>
2025-08-05 18:48:32 -0500

    Update create_model.sql

--- queries/create_model.sql
+++ queries/create_model.sql
@@ -1,1 +1,14 @@
-model creation
+CREATE OR REPLACE MODEL `endless-ripple-465615-b4.penguins_ml.penguins_rf_model`
+OPTIONS(
+  model_type='boosted_tree_classifier',
+  input_label_cols=['species']
+) AS
+SELECT
+  island,
+  culmen_length_mm,
+  culmen_depth_mm,
+  flipper_length_mm,
+  body_mass_g,
+  sex,
+  species
+FROM `endless-ripple-465615-b4.penguins_ml.penguins_data`;
