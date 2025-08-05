3dac742087005b0f27e2806074ff849a7c79eb57
3y3c0d3 <97314133+3y3c0d3@users.noreply.github.com>
2025-08-05 18:50:36 -0500

    Update predict.sql

--- queries/predict.sql
+++ queries/predict.sql
@@ -1,1 +1,14 @@
-prediction sql
+SELECT
+  predicted_species,
+  predicted_species_probs,
+  *
+FROM ML.PREDICT(MODEL `endless-ripple-465615.penguins_ml.penguins_rf_model`, (
+  SELECT
+    island,
+    culmen_length_mm,
+    culmen_depth_mm,
+    flipper_length_mm,
+    body_mass_g,
+    sex
+  FROM `endless-ripple-465615.penguins_ml.penguins_data`
+));
